74dcb06836b3e54e8ffcc9f597c6a9981d1f28f1
Leo Tse <leotse1983@gmail.com>
2013-04-03 23:29:25 -0400

    Added refresh scheduled task

---
+++ tasks/refresh.js
@@ -0,0 +1,36 @@
+//////////////////
+// Refresh Task //
+//////////////////
+// task to initiate job to crawl azdrama
+// job then gets picked up by the spider worker
+
+// dependencies
+var async = require('async')
+,	models = require('../models')
+,	Job = models.Job;
+
+// constants
+var BASE_URL = "http://azdrama.net"
+,	HK_DRAMA = "/hk-drama"
+,	DEFAULT_SELECTOR = "#m .content a";
+
+// create crawler job
+async.series([
+
+	// remove old jobs
+	function(done) { Job.collection.drop(done); },
+	function(done) { Job.ensureIndexes(done); },
+
+	// now add the entry job!
+	function(done) {
+		var url = BASE_URL + HK_DRAMA
+		,	selector = DEFAULT_SELECTOR;
+		Job.create(url, selector, done);
+	}
+
+], function(err) {
+
+	if(err) console.log('refresh task error - ' + err);
+	else console.log('refresh started');
+
+});
